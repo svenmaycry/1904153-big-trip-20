9f663ac0cb9d007e432b1a9191cf14bb2c122c14
SvenMayCry <svenmaycry@gmail.com>
2023-05-19 14:18:08 +0300

    добавляет работу с одной формой и избранным

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,4 +1,5 @@
 const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + min;
+const updateItem = (items, update) => items.map((item) => item.id === update.id ? update : item);
 
-export { getRandomArrayElement, getRandomInteger };
+export { getRandomArrayElement, getRandomInteger, updateItem };
