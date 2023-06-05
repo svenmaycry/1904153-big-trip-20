8c14c746266fe4ef20043a2499f83c271e45aa3c
SvenMayCry <svenmaycry@gmail.com>
2023-06-05 11:56:29 +0300

    реализует фильтрацию + работу с точками

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,5 +1,4 @@
 const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + min;
-const updateItem = (items, update) => items.map((item) => item.id === update.id ? update : item);
 
-export { getRandomArrayElement, getRandomInteger, updateItem };
+export { getRandomArrayElement, getRandomInteger };
